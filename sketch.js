36bcf579ba614c5dd9a18e809aa42e33cdd4d30d
Derick Ostrenko <dostrenko@gmail.com>
2017-11-13 10:49:36 -0600

    floor something

--- sketch.js
+++ sketch.js
@@ -1,24 +1,21 @@
 var result;
+var compass;
 function preload() {
   result = loadStrings('../data/brew_quotes.txt');
+  compass = loadImage("../data/compass.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
   rectMode(CENTER);
+  imageMode(CENTER);
   smooth();
 }
 
 function draw() {
-
   background(h, s, l);  
-
-  textAlign(CENTER);
- 
-  textSize(24); 
-  fill(200);
-  text(result[q], windowWidth/2, windowHeight/2, windowWidth/2, windowHeight/2);
+  image(compass, x, y);
 
 }
 
